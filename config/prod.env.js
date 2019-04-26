b73c604feb2e930f97e93cec1c779e84b9d0ed64
unknown <40466911@qq.com>
2019-04-26 08:50:51 +0800

    在马云上使用测试环境的数据

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,5 +2,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
-  BASE_API: '`http://132.232.94.151:3000/admin`'
+  BASE_API: `"http://132.232.87.95:3000/admin"`
 }
