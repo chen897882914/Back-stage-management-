5cbd09ea2827390d0840ff8d2963de27ee94a263
huruqing <40466911@qq.com>
2019-04-26 08:34:53 +0800

    修改http.js

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,73 +1,73 @@
-import axios from 'axios';
-import { Message } from 'element-ui';
+import axios from "axios";
+import { Message } from "element-ui";
 
-const env = 'test';
+const env = "dev";
 // 设置基本路径
 switch (env) {
-  case 'dev':
-    axios.defaults.baseURL = 'http://192.168.1.67:3000/admin';
-    break
-  case 'test':
-    axios.defaults.baseURL = 'http://132.232.87.95:3000/admin';
-    break
-  case 'prod':
+  case "dev":
+    axios.defaults.baseURL = "http://127.0.0.1:3000/admin";
+    break;
+  case "test":
+    axios.defaults.baseURL = "http://132.232.87.95:3000/admin";
+    break;
+  case "prod":
   // todo
 }
 
 const get = (url, params) => {
-  params = params || {}
+  params = params || {};
   return new Promise((resolve, reject) => {
     axios
       .get(url, {
         params
       })
       .then(res => {
-        if (res.data.code === '666') {
-          resolve(res.data)
+        if (res.data.code === "666") {
+          resolve(res.data);
         } else {
           Message({
             message: res.data.msg,
-            type: 'error',
+            type: "error",
             duration: 5 * 1000
-          })
+          });
         }
       })
       .catch(error => {
         Message({
-          message: '网络异常',
-          type: 'error',
+          message: "网络异常",
+          type: "error",
           duration: 5 * 1000
-        })
-      })
-  })
+        });
+      });
+  });
 };
 const post = (url, params) => {
-  params = params || {}
+  params = params || {};
   return new Promise((resolve, reject) => {
     axios
       .post(url, params)
       .then(res => {
-        if (res.data.code === '666') {
-          resolve(res.data)
+        if (res.data.code === "666") {
+          resolve(res.data);
         } else {
           Message({
             message: res.data.msg,
-            type: 'error',
+            type: "error",
             duration: 5 * 1000
-          })
+          });
         }
       })
       .catch(error => {
         Message({
-          message: '网络异常',
-          type: 'error',
+          message: "网络异常",
+          type: "error",
           duration: 5 * 1000
-        })
-      })
-  })
+        });
+      });
+  });
 };
 
 export default {
   get,
   post
-}
+};
--- src/views/city/edit.vue
+++ src/views/city/edit.vue
@@ -5,7 +5,7 @@
     </div>
     <el-form ref="form" style="width:60%">
       <el-form-item label="城市Id" label-width="120px">
-        <el-input v-model="cityId" placeholder=""/>
+        <el-input v-model="cityId" placeholder/>
       </el-form-item>
       <el-form-item label="城市名称" label-width="120px">
         <el-input v-model="name" placeholder="请输入城市名称"/>
@@ -18,20 +18,20 @@
         <el-switch v-model="isHot" :active-text="isHot?'是':'否'"/>
       </el-form-item>
       <el-form-item style="text-align: center;">
-        <el-button v-loading="loading" type="primary" @click="submit">添加</el-button>
+        <el-button v-loading="loading" type="primary" @click="submit">提交</el-button>
       </el-form-item>
     </el-form>
   </el-card>
 </template>
 
 <script type="text/javascript">
-import $http from '@/utils/http';
+import $http from "@/utils/http";
 export default {
   data() {
     return {
-      cityId: '',
-      name: '',
-      pinyin: '',
+      cityId: "",
+      name: "",
+      pinyin: "",
       isHot: false,
       loading: false
     };
@@ -42,19 +42,36 @@ export default {
   },
   methods: {
     getCityDetail() {
-      const url = '/city/findById';
+      const url = "/city/findById";
       const data = {
         cityId: this.cityId
       };
       $http.post(url, data).then(res => {
-        this.name = res.data.city.name;
-        this.pinyin = res.data.city.pinyin;
-        this.isHot = !!res.data.city.isHot;
+        this.name = res.city.name;
+        this.pinyin = res.city.pinyin;
+        this.isHot = !!res.city.isHot;
         this.loading = false;
       });
     },
     submit() {
       this.loading = true;
+      let url = "/city/edit";
+      let data = {
+        cityId: this.cityId,
+        name: this.name,
+        pinyin: this.pinyin,
+        isHot: this.isHot
+      };
+      $http
+        .post(url, data)
+        .then(res => {
+          this.loading = false;
+          console.log(res);
+        })
+        .catch(err => {
+          console.log(err);
+          this.loading = false;
+        });
     }
   }
 };
